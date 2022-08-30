6e69e1d1265604c12ddbf1ec1a2030449cea4dac
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-08-30 11:16:28 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+  let numbers = result.input.split(" ");
+    let arr = [];
+    for (let i = 0; i < array.length - 1; i += 2) {
+        const element = parseInt(numbers[i]);
+        const next_element = parseInt(numbers[i + 1]);
+        if (next_element) {
+            arr.push(next_element);
+        }
+        arr.push(element);
+
+
+    }
+    console.log(arr);
   
 });
