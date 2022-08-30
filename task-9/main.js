eb6a2748683721534c42dcbc8a554e5bb4e5e1a5
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-08-30 11:53:05 +0400

    Update main.js

--- task-9/main.js
+++ task-9/main.js
@@ -4,6 +4,12 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let numbers = result.input.split(" ");
+    let arr = [];
+    for (let i = 0; i < numbers.length - 1; i += 2) {
+        let element = numbers[1];
+        arr.unshift(element);
+    }
+   console.log(arr);
   
 });
