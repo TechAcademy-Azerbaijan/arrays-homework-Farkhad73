24ad473c29421b782e269210bde228520c1f0ec8
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-08-30 12:02:17 +0400

    Update main.js

--- task-8/main.js
+++ task-8/main.js
@@ -4,6 +4,14 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
-  
+     let numbers = result.input.split(" ");
+  let max = numbers[0];
+  let max_index = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    let element = parseInt(numbers[1]);
+    if (element >= max) {
+        max = element;
+        max_index = 1
+    }
+  }
 });
