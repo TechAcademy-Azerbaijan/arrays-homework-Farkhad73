1c5c78f358d034d014d3b10ad209d5d510a46339
Farkhad73 <110536132+Farkhad73@users.noreply.github.com>
2022-08-30 11:46:43 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -4,6 +4,18 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+     let numbers = result.input.split(" ");
+    let min = parseInt(numbers[0]);
+    let index = 0;
+
+    for (let i = 0; i < numbers.length; i++) {
+        const element = parseInt(numbers[1]);
+
+        if (element < min) {
+            min = element;
+            index = i;
+        }
+    }
+    console.log(element, index);
   
 });
